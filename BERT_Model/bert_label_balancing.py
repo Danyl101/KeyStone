5a73f9de258ae5eb24402963f42097b8a21eddbe
Danyl S John <danyl.s.john@gmail.com>
2025-08-16 02:05:38 +0530

    BERT Data augmentation

--- BERT_Model/bert_label_balancing.py
+++ BERT_Model/bert_label_balancing.py
@@ -6,14 +6,16 @@ import transformers
 from torch.nn import CrossEntropyLoss
 from transformers import MarianMTModel,MarianTokenizer
 
+device="cuda" if torch.cuda.is_available() else "cpu"
+
 en_fr_model_name = "Helsinki-NLP/opus-mt-en-fr"
 fr_en_model_name = "Helsinki-NLP/opus-mt-fr-en"
 
 tokenizer_en_fr=MarianTokenizer.from_pretrained(en_fr_model_name)
-model_en_fr=MarianMTModel.from_pretrained(en_fr_model_name)
+model_en_fr=MarianMTModel.from_pretrained(en_fr_model_name).to(device)
 
 tokenizer_fr_en=MarianTokenizer.from_pretrained(fr_en_model_name)
-model_fr_en=MarianMTModel.from_pretrained(fr_en_model_name)
+model_fr_en=MarianMTModel.from_pretrained(fr_en_model_name).to(device)
 
 labels=[]
 with open("Datasets/label.csv","r")as csvfile:
@@ -28,7 +30,6 @@ negative_num=counts["negative"]
 neutral_num=counts["neutral"]
 positive_num=counts["positive"]
 
-device="cuda" if torch.cuda.is_available() else "cpu"
 
 class_counts=torch.tensor([negative_num,neutral_num,positive_num],dtype=torch.float)
 class_weights=class_counts/1.0
@@ -37,53 +38,63 @@ criterion=CrossEntropyLoss(weight=class_weights)
 
 def text_acquire():
     content_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','Extracted_Content')
-    
+    all_text=[]
+    all_filename=[]
     for filename in os.listdir(content_dir):
         filepath=os.path.join(content_dir,filename)
-        with open(filepath,"r")as f:
+        with open(filepath,"r",encoding="utf-8")as f:
             text=f.read()
-        return text
+            all_text.append(text)
+            all_filename.append(filename)
+    return all_text,all_filename
     
-def text_encoding(text,max_text=1500,step=1000):
-    for start_idx in range(0,len(text),step):
-        text_snippet=text[start_idx:start_idx+max_text]
-    return text_snippet
+def text_encoding(texts,max_text=1500,step=1000):
+    all_text=[]
+    for text in texts:
+        all_snippets=[]
+        for start_idx in range(0,len(text),step):
+            text_snippet=text[start_idx:start_idx+max_text]
+            all_snippets.append(text_snippet)
+        all_text.append(all_snippets)
+    return all_text
 
-def translate(text):
+def translate(texts):
     backtranslated_chunks = []
-    for chunk in text:
-        # English -> French
-        enc = tokenizer_en_fr(chunk, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
-        fr_out = model_en_fr.generate(**enc)
-        fr_text = tokenizer_en_fr.batch_decode(fr_out, skip_special_tokens=True)
+    for text in texts:
+        single_page_text=[]
+        for chunk in text:
+            # English -> French
+            enc = tokenizer_en_fr(chunk, return_tensors="pt", padding=True, truncation=True, max_length=512)
+            enc = {k: v.to(device) for k, v in enc.items()}
+            fr_out = model_en_fr.generate(**enc)
+            fr_text = tokenizer_en_fr.batch_decode(fr_out, skip_special_tokens=True)
+            torch.cuda.empty_cache()
 
-        # French -> English
-        enc_back = tokenizer_fr_en(fr_text, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
-        en_out = model_fr_en.generate(**enc_back)
-        en_text = tokenizer_fr_en.batch_decode(en_out, skip_special_tokens=True)
+            # French -> English
+            enc_back = tokenizer_fr_en(fr_text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+            enc_back = {k: v.to(device) for k, v in enc_back.items()}
+            en_out = model_fr_en.generate(**enc_back)
+            en_text = tokenizer_fr_en.batch_decode(en_out, skip_special_tokens=True)
+            torch.cuda.empty_cache()
 
-        backtranslated_chunks.append(" ".join(en_text))
+            single_page_text.append(en_text)
+        backtranslated_chunks.append(single_page_text)
+    return backtranslated_chunks
 
-    return " ".join(backtranslated_chunks)
-
-def save_file(content):
-    content_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','Extracted_Content')
-    
-    for filename in content_dir:
-        txt_removed_filename=filename-"txt"
-        changed_filename=txt_removed_filename+"french.txt"
+def save_file(content,filename):
+    for text,name in zip(content,filename):
+        full_text="".join(text)
+        name_only, ext = os.path.splitext(name)  # splits "file.txt" -> ("file", ".txt")
+        changed_filename = name_only + "_french.txt"
         filepath=os.path.join('BERT_Content',changed_filename)
         with open(filepath, "w",encoding="utf-8")as f:
-            f.write(content)
+            f.write(full_text)
 
 if __name__=="__main__":
-    content_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','Extracted_Content')
-    
-    for filename in content_dir:
-        texts=text_acquire()
+        texts,filename=text_acquire()
         encoding=text_encoding(texts)
         translated=translate(encoding)
-        save_file(translated)
+        save_file(translated,filename)
         
     
     
